cac84664959f1312d29c8dde9ec49c765eea38cc
WPHU-nuvoton <WPHU@nuvoton.com>
2021-11-15 09:22:25 +0800

    Initial Commit

---
+++ CloudAWS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloudAWS
+//
+//  Created by MS70MAC on 2021/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
